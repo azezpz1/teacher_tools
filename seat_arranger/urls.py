ae149c85f04c2426b0c1502345c6095f985c8ae9
Anthony Panettiere <apanettiere@fastmail.com>
2024-11-02 18:25:07 -0500

    Add a link to the homepage to go to add class period

--- seat_arranger/urls.py
+++ seat_arranger/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import add_class_period
 
 app_name = "seat_arranger"
 
 urlpatterns = [
-    path("add-class-period/", views.add_class_period, name="add_class_period"),
+    path("add-class-period/", add_class_period, name="add_class_period"),
 ]
